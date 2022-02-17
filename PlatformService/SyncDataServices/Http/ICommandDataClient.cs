ed87474eeffbaa81f3be680d43275a9edfa14bf4
Talal Najam <talalnajam98@gmail.com>
2022-02-17 16:23:11 +0000

    Formatting data client

--- PlatformService/SyncDataServices/Http/ICommandDataClient.cs
+++ PlatformService/SyncDataServices/Http/ICommandDataClient.cs
@@ -4,6 +4,6 @@ namespace PlatformService.SyncDataServices.Http
 {
     public interface ICommandDataClient
     {
-        Task SendPlatformToCommand(PlatformReadDto plat);
+        Task SendPlatformToCommand(PlatformReadDto plat); 
     }
 }
